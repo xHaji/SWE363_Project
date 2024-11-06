598e49cb8584e32f5ad978872452150c65dddac2
Abdullah Haji <abdullah.haji2003@gmail.com>
2024-11-07 00:23:53 +0300

    Fixing files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React Project</h1>
+      <p>This is the start of something amazing!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// src/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h2>My Awesome Header</h2>
+    </header>
+  );
+}
+
+export default Header;
