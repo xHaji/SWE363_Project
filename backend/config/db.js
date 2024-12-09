3b2285b5d5630f036be5b2636a971ac7d590fed8
Abdullah Haji <abdullah.haji2003@gmail.com>
2024-12-09 17:50:49 +0300

    Update the database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = "mongodb+srv://sweproject363:%40QAZwsxEDC2003@jobmatchcluster.y0mu5.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://sweproject363:%40QAZwsxEDC2003@jobmatchcluster.y0mu5.mongodb.net/myDatabase?retryWrites=true&w=majority";
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
